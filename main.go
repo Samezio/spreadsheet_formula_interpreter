9db65d83537ea45d133cdc2ab76af9ff0b928220
Sameer Gupta <sameerg2255@gmail.com>
2024-06-22 17:24:15 +0530

    CLI mode

--- main.go
+++ main.go
@@ -1,70 +1,16 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 
-	"github.com/Samezio/spreadsheet_formula_interpreter/ast"
 	"github.com/Samezio/spreadsheet_formula_interpreter/interpreter"
 	"github.com/Samezio/spreadsheet_formula_interpreter/parser"
 )
 
 func main() {
-	basic_interpreter := interpreter.Interpreter{}
-	//Test 2
-	expression := "4-1.5 * 10 + 10 / 100"
-	if a, err := parser.Parse(expression); err != nil {
-		panic(err)
-	} else {
-		fmt.Println(ast.AST_ToString(a, 0))
-		if data, err := basic_interpreter.Interpret(a); err != nil {
-			panic(err)
-		} else {
-			fmt.Printf("Result: %v\n", data.Value())
-
-		}
-	}
-
-	//Test 3
-	expression = "4-1.5*(10+10)/100"
-	if a, err := parser.Parse(expression); err != nil {
-		panic(err)
-	} else {
-		fmt.Println(ast.AST_ToString(a, 0))
-		if data, err := basic_interpreter.Interpret(a); err != nil {
-			panic(err)
-		} else {
-			fmt.Printf("Result: %v\n", data.Value())
-
-		}
-	}
-
-	//Test 4
-	expression = "4-1.5*(10+10)/100 == 3.9"
-	if a, err := parser.Parse(expression); err != nil {
-		panic(err)
-	} else {
-		fmt.Println(ast.AST_ToString(a, 0))
-		if data, err := basic_interpreter.Interpret(a); err != nil {
-			panic(err)
-		} else {
-			fmt.Printf("Result: %v\n", data.Value())
-
-		}
-	}
-	//Test 5
-	expression = "4-1.5 * 10 + 10 / 100 == -10.9"
-	if a, err := parser.Parse(expression); err != nil {
-		panic(err)
-	} else {
-		fmt.Println(ast.AST_ToString(a, 0))
-		if data, err := basic_interpreter.Interpret(a); err != nil {
-			panic(err)
-		} else {
-			fmt.Printf("Result: %v\n", data.Value())
-
-		}
-	}
-	/*scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(os.Stdin)
 
 	basic_interpreter := interpreter.Interpreter{}
 	fmt.Print(">>")
@@ -81,5 +27,5 @@ func main() {
 			fmt.Println(data.Value())
 		}
 		fmt.Print(">>")
-	}*/
+	}
 }
